9b226b871206960866659b5143ea357854f39b35
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-10-23 00:51:34 +0300

    GetCenterIndex method created

--- Task5.Logic/IndexFinder.cs
+++ Task5.Logic/IndexFinder.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace Task5.Logic
 {
-    public class IndexFinder
+    public static class IndexFinder
     {
+        /// <summary>
+        /// Method finds array index 'i', for which sum of the
+        /// elements from 0 to i - 1 is equal to the sum of the elements
+        /// from i + 1 to array.Length - 1
+        /// </summary>
+        /// <param name="array">An array in which to search</param>
+        /// <returns>Index of the found element of -1 if there is no 
+        /// such element</returns>
+        public static int GetCenterIndex(int[] array)
+        {
+            long sum = 0;
+            long leftSum = 0;
+            foreach (int x in array)
+                sum += x;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (sum - leftSum - array[i] == leftSum)
+                    return i;
+                leftSum += array[i];
+            }
+            return -1;
+        }
     }
 }
