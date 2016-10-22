3b7ec2a7d4a2a1412af0f48fadf1ac254bb7abce
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-10-22 21:52:45 +0300

    Classes created

---
+++ TVset.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Task2
+{
+    public class TVset
+    {
+        public void On()
+        {
+            Console.WriteLine("TVset on");
+        }
+
+        public void Off()
+        {
+            Console.WriteLine("TVset off");
+        }
+    }
+}
