5a0b68cd1544578265daa45080f48052d77b8867
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-10-23 20:49:44 +0300

    documentation for StringManipulator created

--- Task6.Logic/StringManipulator.cs
+++ Task6.Logic/StringManipulator.cs
@@ -6,9 +6,22 @@ using System.Threading.Tasks;
 
 namespace Task6.Logic
 {
+    /// <summary>
+    /// Class that represents methods to manipulate with strings
+    /// </summary>
     public static class StringManipulator
     {
-        private static string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        /// <summary>
+        /// const string variable containing alphabet
+        /// </summary>
+        private const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        /// <summary>
+        /// This method concats two strings, removes duplicate elements
+        /// and sorts remaining elements ascending
+        /// </summary>
+        /// <param name="s1">first string to manipulate with</param>
+        /// <param name="s2">second string to manipulate with</param>
+        /// <returns>result string</returns>
         public static string ConcatUniqueAscending(this string s1, string s2)
         {
             string s = s1 + s2;
