74dfa1278d437b1ac250e6795c2d443b6fba08df
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-10-23 20:56:05 +0300

    Task7.Logic project created. Stub for InsertNumber method added

---
+++ Task7.Logic/NumberManipulator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7.Logic
+{
+    public static class NumberManipulator
+    {
+        public static int InsertNumber(this int x, int y, int start, int end)
+        {
+            return 42;
+        }
+    }
+}
