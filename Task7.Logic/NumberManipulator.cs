677b908738a40ab1ec0337b9c766da2b50af2f03
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-10-23 22:32:51 +0300

    Task7.CUI program completed. Temp version of Insertion added

--- Task7.Logic/NumberManipulator.cs
+++ Task7.Logic/NumberManipulator.cs
@@ -6,15 +6,26 @@ using System.Threading.Tasks;
 
 namespace Task7.Logic
 {
+    /// <summary>
+    /// Class that represents methods to manipulate with numbers
+    /// </summary>
     public static class NumberManipulator
     {
+        /// <summary>
+        /// Inserts y in x. As result takes the position from start to end
+        /// </summary>
+        /// <param name="x">Number in which to insert</param>
+        /// <param name="y">Number which is inserting</param>
+        /// <param name="start"> Start pos to insert from</param>
+        /// <param name="end"> End pos to insert to</param>
+        /// <returns></returns>
         public static int Insertion(this int x, int y, int start, int end)
         {
             int x1 = x;  
-            for (int i = start; i <= end; i++)
+            for (int i = start, j = 0; i <= end; i++, j++)
             {
                 x1 -= (x1 & (1 << i)); //reset bit in the x1
-                x1 += (y & (1 << i)); //add value of bit in the y
+                x1 += (y & (1 << j)) << start; //add value of bit in the y
             }
             return x1;
         }
