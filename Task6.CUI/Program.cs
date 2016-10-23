df263728bc422b14454f6cdd20c624a6f0e3736e
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-10-23 20:24:57 +0300

    Task6.CUI created

---
+++ Task6.CUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task6.Logic;
+
+namespace Task6.CUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = Console.ReadLine();
+            string s2 = Console.ReadLine();
+            Console.WriteLine(s1.ConcatUniqueAscending(s2));
+        }
+    }
+}
