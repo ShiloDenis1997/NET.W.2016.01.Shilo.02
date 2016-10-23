08a8adb6e4028315c4668a6dbcb4a789a3d524d9
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-10-23 20:22:43 +0300

    ConcatUniqueAscending first realization added

--- Task6.Logic/StringManipulator.cs
+++ Task6.Logic/StringManipulator.cs
@@ -8,6 +8,11 @@ namespace Task6.Logic
 {
     public static class StringManipulator
     {
-        
+        private static string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        public static string ConcatUniqueAscending(this string s1, string s2)
+        {
+            string s = s1 + s2;
+            return s.Intersect(alphabet).OrderBy((c) => c).ToString();
+        }
     }
 }
