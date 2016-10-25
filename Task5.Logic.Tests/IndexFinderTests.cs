2833cb12732e32b4d467840a1c3d4c0bafa24bfa
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-10-26 01:17:23 +0300

    First version of DDT test project added.

---
+++ Task5.Logic.Tests/IndexFinderTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+
+namespace Task5.Logic.Tests
+{
+    [TestClass]
+    public class IndexFinderTests
+    {
+        public TestContext TestContext { get; set; }
+
+        [DataSource(
+            "Microsoft.VisualStudio.TestTools.DataSource.XML",
+            "|DataDirectory|\\TestCases.xml",
+            "TestCase",
+            DataAccessMethod.Sequential)]
+        [DeploymentItem(@"Task5.Logic.Tests\TestCases.xml")]
+        [TestMethod]
+        public void GetCenterIndex_()
+        {
+            Debug.WriteLine(AppDomain.CurrentDomain.GetData("APPBASE"));
+            //arrange
+            //var source = TestContext.DataRow["Array"];
+            int expected = Convert.ToInt32(TestContext.DataRow["ExpectedResult"]);
+            //act
+            int actual = IndexFinder.GetCenterIndex(new int[]{ 1, 2});
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
