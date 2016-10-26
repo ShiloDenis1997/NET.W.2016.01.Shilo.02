42cfc5be4382b356686231e309a0a601cd2c4d66
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-10-26 22:51:25 +0300

    nunit installed to Task7.Logic.Tests

--- Task7.Logic.Tests/NumberManipulatorTests.cs
+++ Task7.Logic.Tests/NumberManipulatorTests.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace Task7.Logic.Tests
 {
+    [TestFixture]
     public class NumberManipulatorTests
     {
+        [Test]
+        public void Insertion_drivenData_ExpectedReturn
+            (int x, int y, int start, int end, int expected)
+        {
+            
+        }
     }
 }
