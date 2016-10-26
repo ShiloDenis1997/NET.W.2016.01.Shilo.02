5117624bc605043e01e2688e977e05531c531a49
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-10-26 22:12:15 +0300

    NUnit tests for Task5 completed. ExpectedException udpated.

--- Task5.Logic.NUnitTests/IndexFinderTests.cs
+++ Task5.Logic.NUnitTests/IndexFinderTests.cs
@@ -1,58 +1,61 @@
-﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
+using NUnit.Framework;
+using NUnitExtension;
 
 namespace Task5.Logic.NUnitTests
 {
     [TestFixture]
     public class IndexFinderTests
     {
-        [TestCase(new int[] { 1, 2, 3, 2, 1 }, 2,
+        [TestCase(new[] { 1, 2, 3, 2, 1 }, 2,
             Description = "Center element test")]
-        [TestCase(new int[] { 2, 0, 0 }, 0,
+        [TestCase(new[] { 2, 0, 0 }, 0,
             Description = "Left boundary test")]
-        [TestCase(new int[] { 0, 0, 5 }, 2,
+        [TestCase(new[] { 0, 0, 5 }, 2,
             Description = "Right boundary test")]
-        [TestCase(new int[] { 5, 5, 1, 2, 2}, 1,
+        [TestCase(new[] { 5, 5, 1, 2, 2}, 1,
             Description = "After first element test")]
-        [TestCase(new int[] { 1, 2, 3, 4, 6 }, 3,
+        [TestCase(new[] { 1, 2, 3, 4, 6 }, 3,
             Description = "Before last boundary test")]
-        [TestCase(new int[] { 5 }, 0,
+        [TestCase(new[] { 5 }, 0,
             Description = "One element test")]
-        [TestCase(new int[] { 1, 2, 3, 4 }, -1,
+        [TestCase(new[] { 1, 2, 3, 4 }, -1,
             Description = "Even array without center element")]
-        [TestCase(new int[] { 1, 2, 3, 4, 5 }, -1,
+        [TestCase(new[] { 1, 2, 3, 4, 5 }, -1,
             Description = "Odd array without center element")]
-        [TestCase(new int[] { -10, 20, -5, -15 }, 0,
+        [TestCase(new[] { -10, 20, -5, -15 }, 0,
             Description = "Array with negatives. Left boundary test")]
-        [TestCase(new int[] { -20, 5, 10, -10, -20 }, 1,
+        [TestCase(new[] { -20, 5, 10, -10, -20 }, 1,
             Description = "Array with negatives. After first element test")]
-        [TestCase(new int[] { -15, -5, 20, -10 }, 3,
+        [TestCase(new[] { -15, -5, 20, -10 }, 3,
             Description = "Array with negatives. Right boundary")]
-        [TestCase(new int[] { -20, -10, 10, 5, -20 }, 3,
+        [TestCase(new[] { -20, -10, 10, 5, -20 }, 3,
             Description = "Array with negatives. Before last element test")]
-        [TestCase(new int[] { -20, -10, -5, -10, -20 }, 2,
+        [TestCase(new[] { -20, -10, -5, -10, -20 }, 2,
             Description = "Array with negatives. Center element test")]
-        [TestCase(new int[] { -1, -2, -3, -4 }, -1,
+        [TestCase(new[] { -1, -2, -3, -4 }, -1,
             Description = "Even array with negatives without center element")]
-        [TestCase(new int[] { -1, -2, -3 }, -1,
+        [TestCase(new[] { -1, -2, -3 }, -1,
             Description = "Odd array with negatives without center element")]
-        [TestCase(new int[] { -2 }, 0,
+        [TestCase(new[] { -2 }, 0,
             Description = "One negative element test")]
-        [TestCase(null, 0, ,
-            Description = "Null array exception expected")]
         [Test]
         public void GetCenterIndex_Array_ExcpectedArg2
             (int[] array, int expected)
         {
-            int actual;
             //act
-            actual = IndexFinder.GetCenterIndex(array);
+            int actual = array.GetCenterIndex();
             //assert
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(null, Description = "Array null reference test")]
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetCenterIndex_Null_ExcpectedArgumentNullException(int[] array)
+        {
+            var centerIndex = array.GetCenterIndex();
+            Assert.Fail($"Exception excpected {centerIndex} returned");
         }
     }
 }
